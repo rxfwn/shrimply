1f2a2ca413f5165327b0db25db681d05d350b0d6
rxfwn <marie.cruciani02@gmail.com>
2026-03-05 15:49:37 +0100

    init: setup React + Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-orange-50 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-orange-500">
+        🦐 Shrimply fonctionne !
+      </h1>
+    </div>
+  )
+}
+
+export default App
